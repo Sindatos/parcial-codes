75727e4e43ccf14028438e05757c1c296c07d932
Jaime Gamba <95046644+Sindatos@users.noreply.github.com>
2024-08-27 19:58:21 -0500

    Create ecuacion.py

---
+++ ecuacion.py
@@ -0,0 +1,7 @@
+import math
+x = 1
+y = 1
+a = (pow(math.sqrt(5*x + ((math.sqrt(pow(y, x-1)))/(y+2*x))),3))
+b = ((pow(pow(x,2),3))/(pow(y,3)*x+1)-3 * (math.sqrt(3+math.sqrt(x-1))))
+z = a-b
+print(z)
